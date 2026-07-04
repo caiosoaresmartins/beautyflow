2efa705a1c10510b697af5341ce0a0f24efae530
Caio Soares <105523930+caiosoaresmartins@users.noreply.github.com>
2026-07-04 15:41:52 -0300

    feat: add update-salon DTO

---
+++ src/salons/dto/update-salon.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UpdateSalonDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  timezone?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  address?: string;
+}
