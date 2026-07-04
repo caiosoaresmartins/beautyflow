f8f670a6bc9e79dfd349debfd3143c598c00e64e
Caio Soares <105523930+caiosoaresmartins@users.noreply.github.com>
2026-07-04 15:35:26 -0300

    feat: add JWT strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      sub: payload.sub,
+      salonId: payload.salonId,
+      role: payload.role,
+      email: payload.email,
+    };
+  }
+}
